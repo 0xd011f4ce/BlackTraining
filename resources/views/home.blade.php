1b601585cf1c25a9fb6849afcacc1703cf93f4a3
lain <lyx@lyxincyberia.org>
2024-09-10 21:08:12 -0500

    updated the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,14 @@
 @section('title', 'Home')
 
 @section('content')
+    <h1>Home</h1>
 
+    <p>Welcome to BlackTraining, a free cybersecurity training platform.</p>
+    <p>Our goal is to provide quality training to anyone interested in cybersecurity, regardless of their background or
+        financial situation.</p>
+    <p>Here, you can access courses, participate in forums and image boards, all licensed under CC-BY-SA unless stated
+        otherwise. Create an account to join the community and access all content at no cost.</p>
+    <p>Donations are optional but appreciated, as they help cover server costs. You can also support us by contributing to
+        the free (as in freedom) code, bookmarking our site, or spreading the word.</p>
+    <p>Disclaimer: We are not responsible for how the information provided here is used.</p>
 @endsection
