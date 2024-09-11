c940138eb9d55cadebaaeb584638bb5650b224c2
lain <lyx@lyxincyberia.org>
2024-09-10 21:02:58 -0500

    Now pages can be shown

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Page;
 use Illuminate\Http\Request;
+use Parsedown;
 
 class PageController extends Controller
 {
     public function show(Request $request, Page $page)
     {
-        dd($page);
+        $content = Parsedown::instance()->text($page->content);
+
+        return view("page", compact("page", "content"));
     }
 }
---
+++ resources/views/page.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', $page->name)
+
+@section('content')
+    {!! $content !!}
+@endsection
